45671993db43df6847bed9b5d6ee23bb198a1a62
Jari Siiskonen <siiskone@gmail.com>
2012-04-29 09:33:08 +0300

    Testi

---
+++ src/Morse.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jari
+ *
+ */
+public class Morse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Morse again");
+	}
+
+}
